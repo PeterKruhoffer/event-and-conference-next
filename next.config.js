ebcf4ad6516b3e4d87b32c0ee54b418490ed6dc5
peterkruhoffer <peter.kruhoffer@gmail.com>
2026-06-05 14:14:26 +0200

    Fix images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,34 @@
 /** @type {import('next').NextConfig} */
+const drupalBaseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL
+const drupalBaseUrlWithoutTrailingSlash = drupalBaseUrl?.replace(/\/$/, "")
+const imageDomain = process.env.NEXT_IMAGE_DOMAIN
+  ? new URL(`https://${process.env.NEXT_IMAGE_DOMAIN.replace(/^https?:\/\//, "").replace(/\/$/, "")}`).hostname
+  : undefined
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
+    remotePatterns: imageDomain
+      ? [
+          {
+            protocol: "https",
+            hostname: imageDomain,
+            pathname: "/sites/default/files/**",
+          },
+        ]
+      : [],
+  },
+  async rewrites() {
+    if (!drupalBaseUrlWithoutTrailingSlash) {
+      return []
+    }
+
+    return [
       {
-        // protocol: 'https',
-        hostname: process.env.NEXT_IMAGE_DOMAIN,
-        // port: '',
-        // pathname: '/sites/default/files/**',
+        source: "/sites/default/files/:path*",
+        destination: `${drupalBaseUrlWithoutTrailingSlash}/sites/default/files/:path*`,
       },
-    ],
+    ]
   },
 }
 
